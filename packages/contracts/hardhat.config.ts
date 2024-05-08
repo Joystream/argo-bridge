32fb433a73c0c7e939cc7310684db0fc4300df29
Klaudiusz Dembler <accounts@kdembler.com>
2024-05-08 15:31:18 +0200

    add contract verification and Sepolia deployment

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,11 +1,24 @@
 import "@typechain/hardhat"
 import "@nomicfoundation/hardhat-ethers"
 import "@nomicfoundation/hardhat-chai-matchers"
-import { HardhatUserConfig } from "hardhat/config"
+import { HardhatUserConfig, vars } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY")
+const INFURA_API_KEY = vars.get("INFURA_API_KEY")
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY")
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 }
 
 export default config
