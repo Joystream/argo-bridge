c72e48cd6f4adbd816ab971c712a700fb186368a
Klaudiusz Dembler <accounts@kdembler.com>
2024-07-22 13:05:56 +0200

    fix build issues

--- packages/app/src/pages/Governance/proposals/ProposalsTab.tsx
+++ packages/app/src/pages/Governance/proposals/ProposalsTab.tsx
@@ -3,7 +3,5 @@ import { useEvmProposalsQuery } from '@/lib/hooks'
 import { ProposalsTable } from './ProposalsTable'
 
 export const ProposalsTab: FC = () => {
-  const { data, isLoading } = useEvmProposalsQuery()
-
-  return <ProposalsTable proposals={data ?? []} isLoading={isLoading} />
+  return <ProposalsTable />
 }
--- packages/app/src/pages/Transfers/TransfersPage.tsx
+++ packages/app/src/pages/Transfers/TransfersPage.tsx
@@ -1,20 +1,17 @@
 import { FC } from 'react'
 import { TransfersTable } from './TransfersTable'
 import { Outlet } from 'react-router-dom'
-import { useTransfersQuery } from '@/lib/hooks'
 import { useSafeStore } from '@/providers/safe/safe.store'
 import { usePendingOperatorCallsQuery } from '@/providers/safe/safe.hooks'
 
 export const TransfersPage: FC = () => {
-  const { data } = useTransfersQuery()
-
   const { safeApiKit } = useSafeStore()
   // prefetch pending safe operations
   usePendingOperatorCallsQuery(safeApiKit)
 
   return (
     <div>
-      <TransfersTable transfers={data || []} />
+      <TransfersTable />
       <Outlet />
     </div>
   )
