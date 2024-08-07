6169646a9f12af503a8208e48fadd749addd6f3a
Klaudiusz Dembler <accounts@kdembler.com>
2024-08-07 23:03:40 +0200

    report unfinalized height in processor status

--- packages/indexer/src/server-extension/resolvers/index.ts
+++ packages/indexer/src/server-extension/resolvers/index.ts
@@ -1,22 +1,69 @@
 import 'reflect-metadata'
-import { Query, Resolver } from 'type-graphql'
+import { Field, ObjectType, Query, Resolver } from 'type-graphql'
 import type { EntityManager } from 'typeorm'
+
+@ObjectType()
+export class SquidStatus {
+  @Field(() => Number, { nullable: false })
+  height!: number;
+
+  @Field(() => Number, { nullable: false })
+  finalizedHeight!: number;
+
+  constructor(props: Partial<SquidStatus>) {
+    Object.assign(this, props);
+  }
+}
 
 @Resolver()
 export class ProcessorStatusResolver {
   constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => Number)
-  async baseProcessorStatus(): Promise<number> {
+  @Query(() => SquidStatus)
+  async baseProcessorStatus(): Promise<SquidStatus> {
     const manager = await this.tx()
-    const result = await manager.query(`SELECT height FROM eth_processor.status WHERE id = 0`)
-    return result[0]?.height
+    return await manager
+      .query(
+        `
+        SELECT f.height AS "finalizedHeight",
+            COALESCE(h.height, f.height) AS height
+        FROM squid_processor.status AS f
+        FULL JOIN
+            (SELECT *
+                FROM eth_processor.hot_block
+                ORDER BY height DESC
+                LIMIT 1) AS h ON TRUE
+        WHERE f.id = 0
+        `,
+      )
+      .then((r) => {
+        const height = parseInt(r[0].height);
+        const finalizedHeight = parseInt(r[0].finalizedHeight);
+        return new SquidStatus({ height, finalizedHeight });
+      });
   }
 
-  @Query(() => Number)
-  async joyProcessorStatus(): Promise<number> {
+  @Query(() => SquidStatus)
+  async joyProcessorStatus(): Promise<SquidStatus> {
     const manager = await this.tx()
-    const result = await manager.query(`SELECT height FROM joy_processor.status WHERE id = 0`)
-    return result[0]?.height
+    return await manager
+      .query(
+        `
+        SELECT f.height AS "finalizedHeight",
+            COALESCE(h.height, f.height) AS height
+        FROM squid_processor.status AS f
+        FULL JOIN
+            (SELECT *
+                FROM eth_processor.hot_block
+                ORDER BY height DESC
+                LIMIT 1) AS h ON TRUE
+        WHERE f.id = 0
+        `,
+      )
+      .then((r) => {
+        const height = parseInt(r[0].height);
+        const finalizedHeight = parseInt(r[0].finalizedHeight);
+        return new SquidStatus({ height, finalizedHeight });
+      });
   }
 }
