5187744117914ee722f1cd4e63c8069efd0bd94c
Klaudiusz Dembler <accounts@kdembler.com>
2024-08-20 16:31:57 +0200

    [app] display signers js member name

--- packages/app/src/components/AddressLink.tsx
+++ packages/app/src/components/AddressLink.tsx
@@ -5,10 +5,33 @@ import { FC } from 'react'
 import { isAddress } from 'viem'
 import { useConfig } from 'wagmi'
 
-const knownAddresses = {
+const knownSigners = {
+  j4rxspbqxnje2ybkbxqsvrzx8zwjpehadevnyrawic4i22art: 'leet_joy',
+  j4u3vjtyw3f4au2uvvr6hl264pjkfkmguygmtejdjhqfgjtco: 'klaudiusz',
+  j4sbzarjmedzccfzfktxpqz7uldsz3uyt3vfqivsdfeghcpgf: '0x2bc',
+  j4w2ls9mxl5kqmpg5dnjgozankhrgrthsgcyp42gfxcykrv5s: 'tomato',
+  j4rk5azus8jsxdjr8e33d5egofwdfvk7zu26zjebwq7lu3ldb: 'freakstatic',
+  j4wdpywnvexfjh7wyfjrtvj8dvt9mjdedhoa57a2kcsngvtf2: 'sieemma',
+  j4vtgdn3jpp5yidvvynasmsufe3e5dhqjc4gd6t6cdgdrgwas: 'marat_mu',
+  '0x34591a1e3f6394208fef7ef433c89f1d793d8d8d': 'klaudiusz',
+  '0xf2052f276d33e4b6655aa193e5bd1cc9cc3f1b00': 'leet_joy',
+  '0xaa709d6b0c4e0f10cb49203dcd7442e5626cbd53': 'tomato',
+  '0x75996a4c2683ef365549ee5ee39ed187f239940d': '0x2bc',
+  '0x2b6626fc4bb515239b65b36368c4a271cb06d3f4': 'sieemma',
+  '0x28aa4f9ffe21365473b64c161b566c3cdead0108': 'fierydev',
+  '0x38c38e86f747a01e1520135193401b61dfe726b9': 'maxlevush',
+  '0xbe3f7220f73aa3009a67e1a1193454f6099c9b11': 'spat_sochi',
+  '0x9132dadb3da875301dab5de3ff42b015ec8956ca': 'l1dev',
+  '0x0e835f8925f72b3220db3a9305b2a2c096329b62': 'igrex',
+  '0x078fa53aa7d2c45b4af150701d83ea66b73a0364': 'marat_mu',
+  '0x76c4fb9feb3a9b11bf99b7c31ebad30de4548db8': 'Codefikeyz',
+}
+
+const knownAddresses: Record<string, string> = {
   [EVM_NETWORK.contracts.timelock.toLowerCase()]: 'TimelockController',
   [EVM_NETWORK.contracts.bridge.toLowerCase()]: 'ArgoBridgeV1',
   [EVM_NETWORK.contracts.erc20.toLowerCase()]: 'JoystreamERC20',
+  ...knownSigners,
 }
 
 if (EVM_NETWORK.opMulti) {
