ee29556691576fc8385416b35308de75d94b2cfd
Klaudiusz Dembler <accounts@kdembler.com>
2024-08-26 14:26:33 +0200

    [app] fix walletconnect for safe signers

--- packages/app/src/providers/safe/safe.manager.ts
+++ packages/app/src/providers/safe/safe.manager.ts
@@ -4,10 +4,12 @@ import { useUser } from '@/providers/user/user.hooks'
 import SafeApiKit from '@safe-global/api-kit'
 import Safe from '@safe-global/protocol-kit'
 import { FC, useEffect, useRef } from 'react'
+import { useAccount } from 'wagmi'
 
 export const SafeManager: FC = () => {
   const { adminMulti, opMulti } = EVM_NETWORK
   const { userEvmAdmin, userEvmOperator } = useUser()
+  const { connector } = useAccount()
 
   const { safeApiKit, setOperatorSafe, setAdminSafe, setSafeApiKit } =
     useSafeStore()
@@ -25,28 +27,38 @@ export const SafeManager: FC = () => {
   }, [safeApiKit])
 
   useEffect(() => {
-    if (adminMulti && !adminSafePromise.current && userEvmAdmin) {
-      adminSafePromise.current = Safe.init({
-        provider: window.ethereum,
-        signer: userEvmAdmin,
-        safeAddress: adminMulti.address,
-      })
-      adminSafePromise.current.then((safe) => {
+    if (adminMulti && !adminSafePromise.current && userEvmAdmin && connector) {
+      const setup = async () => {
+        const safe = await Safe.init({
+          provider: (await connector.getProvider()) as any,
+          signer: userEvmAdmin,
+          safeAddress: adminMulti.address,
+        })
         setAdminSafe(safe)
-      })
+        return safe
+      }
+
+      adminSafePromise.current = setup()
     }
   }, [adminMulti, userEvmAdmin])
 
   useEffect(() => {
-    if (opMulti && !operatorSafePromise.current && userEvmOperator) {
-      operatorSafePromise.current = Safe.init({
-        provider: window.ethereum,
-        signer: userEvmOperator,
-        safeAddress: opMulti.address,
-      })
-      operatorSafePromise.current.then((safe) => {
+    if (
+      opMulti &&
+      !operatorSafePromise.current &&
+      userEvmOperator &&
+      connector
+    ) {
+      const setup = async () => {
+        const safe = await Safe.init({
+          provider: (await connector.getProvider()) as any,
+          signer: userEvmOperator,
+          safeAddress: opMulti.address,
+        })
         setOperatorSafe(safe)
-      })
+        return safe
+      }
+      operatorSafePromise.current = setup()
     }
   }, [opMulti, userEvmOperator])
 
