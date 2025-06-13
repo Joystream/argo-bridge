acdcdbd0533f4188037aaa35e61ecc2de29c540e
Klaudiusz Dembler <accounts@kdembler.com>
2025-06-13 11:39:29 +0200

    [app] replace subscan links

--- packages/app/src/components/JoyTxLink.tsx
+++ packages/app/src/components/JoyTxLink.tsx
@@ -9,7 +9,7 @@ export const JoyTxLink: FC<{ hash: string; block: number }> = ({
 }) => {
   const isMainnet = JOY_NETWORK.name === 'Joystream'
   const url = isMainnet
-    ? `https://joystream.subscan.io/extrinsic/${hash}`
+    ? `https://explorer.joystream.org/#/extrinsics/${hash}`
     : `https://polkadot.js.org/apps/?rpc=${encodeURI(JOY_NETWORK.rpc.url)}#/explorer/query/${block}`
 
   const truncated = truncateValue(hash, 10)
